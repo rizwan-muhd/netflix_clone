cbc63b785de1fd271e78a82eb7ca917df387037b
rizwan-muhd <muhdrizwan550@gmail.com>
2022-04-09 08:27:03 +0530

    navbar created

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './banner.css'
+function Banner() {
+  return (
+    <div className='banner'>
+      <div className="condent">
+        <h1 className='movie-title'>Money Hiest</h1>
+        <div className="buttons">
+          <button>Play</button>
+          <button>My list</button>
+        </div>
+        <p className='movie-description'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been <br/> the industry's standard dummy text ever since the 1500s, </p>
+      </div>
+      </div>
+  )
+}
+
+export default Banner
